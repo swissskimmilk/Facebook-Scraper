4b03ae0b6387a83b10114f14236ee9a167d53d81
Aidan Rickert <64725899+swissskimmilk@users.noreply.github.com>
2021-06-16 13:16:08 -0700

    Add files via upload

--- Scraper.py
+++ Scraper.py
@@ -2,9 +2,12 @@ from facebook_scraper import get_posts
 from facebook_scraper import get_profile
 from datetime import datetime
 import mysql.connector
+import requests
 
 # Declare & initialize constants
 databaseName = "facebook_scraper_data"
+citiesInDistrict = ['palo alto', 'stanford', 'los altos', 'woodside', 'mountain view', 'los altos hills', 'campbell',
+                    'saratoga', 'los gatos', 'scotts valley', 'menlo park', 'redwood city', 'san jose', 'santa cruz']
 
 database = mysql.connector.connect(
     host="localhost",
@@ -66,20 +69,23 @@ except mysql.connector.errors.ProgrammingError:
 # Create profiles table, pass if already exists
 try:
     cursor.execute("CREATE TABLE profiles ("
-                   "profile_id int NOT NULL AUTO_INCREMENT,"
-                   "comment_id int NOT NULL,"
+                   "profile_id BIGINT NOT NULL,"
+                   "fetched_time DATETIME,"
+                   "profile_url text,"
                    "name text,"
                    "about text,"
                    "education text,"
                    "favorite_quotes text,"
                    "location text,"
+                   "in_district BOOLEAN,"
                    "work text,"
+                   "other_info text,"
                    "PRIMARY KEY (profile_id))")
 except mysql.connector.errors.ProgrammingError:
     pass
 
 # Get posts
-posts = get_posts('repannaeshoo', pages=1, options={"progress": True, "comments": True, "reactors": True},
+posts = get_posts('repannaeshoo', pages=1, options={"progress": True, "comments": True},
                   credentials=("josephmayes97@gmail.com", "nHi5&UcFzk6i"))
 for post in posts:
     print(post)
@@ -97,6 +103,8 @@ for post in posts:
         print("No comments on post")
     else:
         for comment in post['comments_full']:
+            print(comment)
+
             # Get the number of replies
             if 'replies' in comment:
                 repliesCount = len(comment['replies'])
@@ -111,3 +119,58 @@ for post in posts:
                  comment['comment_url'], comment['commenter_name'], comment['commenter_id'], comment['comment_time'],
                  comment['comment_text'], repliesCount)
             )
+            database.commit()
+
+            try:
+                # Get profile of commenter
+                profile = get_profile(comment['commenter_id'])
+                print(profile)
+            except Exception as e:
+                print(e)
+
+            # Get current city and compare against cities in the district
+            placesLived = profile.get('Places Lived')
+            if placesLived is not None:
+                currentLocation = placesLived[:profile.get('Places Lived').index(",")]
+                currentLocation = currentLocation.lower()
+                if currentLocation in citiesInDistrict:
+                    inDistrict = True
+                else:
+                    inDistrict = False
+            else:
+                currentLocation = None
+                inDistrict = False
+
+            # Process URL
+            if profile.get('Contact Info') is not None and profile.get('Contact Info').get('Facebook') is not None:
+                profileURL = profile.get('Contact Info').get('Facebook')
+            else:
+                profileURL = None
+
+            # Check for error getting profile
+            if profile.get('Name') == "You Can't Use This Feature Right Now":
+                name = "Error"
+                cursor.execute(f"SELECT * FROM profiles WHERE profile_id = {comment['commenter_id']}")
+                entries = cursor.fetchall()
+                print("Fetchall: " + str(entries) + " Length: " + str(len(entries)))
+                if len(entries) == 0:
+                    # Insert profile data in profiles table
+                    cursor.execute(
+                        "INSERT INTO profiles (profile_id, fetched_time, profile_url, name, about, education, favorite_quotes, "
+                        "location, in_district, work, other_info) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                        (comment['commenter_id'], datetime.now().strftime('%Y-%m-%d %H:%M:%S'), profileURL, name, profile.get('About'),
+                         profile.get('Education'), profile.get('Favorite Quotes'),
+                         currentLocation, inDistrict, profile.get('Work'), profile.get('Basic Info'))
+                    )
+                    database.commit()
+            else:
+                name = profile.get('Name')
+                # Insert profile data in profiles table
+                cursor.execute(
+                    "REPLACE INTO profiles (profile_id, fetched_time, profile_url, name, about, education, favorite_quotes, "
+                    "location, in_district, work, other_info) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (comment['commenter_id'], datetime.now().strftime('%Y-%m-%d %H:%M:%S'), profileURL, name, profile.get('About'),
+                     profile.get('Education'), profile.get('Favorite Quotes'),
+                     currentLocation, inDistrict, profile.get('Work'), profile.get('Basic Info'))
+                )
+                database.commit()
